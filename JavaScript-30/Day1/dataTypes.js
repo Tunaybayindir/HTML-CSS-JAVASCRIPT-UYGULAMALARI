14df1081f289a11a2efebd5fa1cf868469c8d5fd
Tunay Bayındır <127817130+Tunaybayindir@users.noreply.github.com>
2023-12-20 17:28:25 +0300

    ilk gün

---
+++ JavaScript-30/Day1/dataTypes.js
@@ -0,0 +1,18 @@
+console.log("Data Types")
+
+console.log("isim " +(typeof "isim"));
+console.log("rakam " +(typeof 5));
+console.log("doğru-yanlis " +(typeof true));
+console.log("null " +(typeof null));
+console.log("undefined " +(typeof undefined));
+
+let youtubeChannel = "prototurk",
+youtubeChannelAge = 11,
+isChannelStillActive = true,
+secondChannel = null,
+channelMoney = undefined
+
+const PI = 3.14
+const GRAVITY = 9.81
+
+console.log(youtubeChannel,youtubeChannelAge,isChannelStillActive,secondChannel,channelMoney)
